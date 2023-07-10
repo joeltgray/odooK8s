37b537fe10153b88535cba7f268c1e3004d7e6d0
Joel Gray <joeltgray@gmail.com>
2023-07-10 15:49:46 +0100

    Reduce timeout, helm is fast af

--- build/odoo_helm_reinstall.sh
+++ build/odoo_helm_reinstall.sh
@@ -4,8 +4,8 @@ echo "Uninstalling Helm charts from microk8s cluster..."
 cd ../helm
 helm uninstall odoo -n odoo
 
-echo "Sleeping for 30 seconds to allow pods to terminate..."
-sleep 30
+echo "Sleeping for 10 seconds to allow pods to terminate..."
+sleep 10
 
 echo "Reinstalling Helm charts to microk8s cluster..."
 helm install odoo . -n odoo
